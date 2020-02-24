1d7fc8f03425161d5f3cbd40a2388f18e9aa2c42
Lázaro Nunes de Macêdo Júnior <lazarojunior@gmail.com>
2020-02-24 14:41:42 -0300

    Novos arquivos

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+import logo from '../'
+// import { Container } from './styles';
+
+export default function Feed() {
+  return (
+    <View />
+  );
+}
