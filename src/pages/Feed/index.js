c741c1b09c626161dae644abecc8d5e7bf82aad1
Lázaro Nunes de Macêdo Júnior <lazarojunior@gmail.com>
2020-02-25 00:16:20 -0300

    Pagina dos itens

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -6,21 +6,34 @@ import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
 
 export default function Feed() {
     const [feed, setFeed] = useState([])
+    const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(false)
+
+    async function loadPage(pageNumber = page) {
+        if(total && pageNumber > total) return;
+
+        setLoading(true)
+
+        const response = await fetch(
+            `https://my-json-server.typicode.com/lazarojunior/rs-insta-clone/feed?_expand=author&_limit=5&_page=${pageNumber}`
+        )
+
+        const data = await response.json()
+        const totalItems = response.headers.get('X-Total-Count')
+
+        // console.warn(data);
+        console.log(pageNumber);
+
+        setTotal(Math.floor(totalItems / 5))
+        setFeed([...feed, ...data])
+        setPage(pageNumber + 1)
+        setLoading(false)
+    }
 
     useEffect(() => {
-        async function loadFeed() {
-            const response = await fetch(
-                'https://my-json-server.typicode.com/lazarojunior/rs-insta-clone/feed?_expand=author&_limit=5&_page=1'
-            )
-
-            const data = await response.json()
-            console.warn(data);
-
-            setFeed(data)
-        }
-
-        loadFeed()
-    })
+        loadPage()
+    }, [])
 
     // loadFeed()
 
@@ -29,6 +42,9 @@ export default function Feed() {
             <FlatList
                 data={feed}
                 keyExtractor={post => String(post.id)}
+                onEndReached={() => loadPage()}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={loading && <Loading /> }
                 renderItem={({ item }) => (
                     <Post>
                         <Header>
