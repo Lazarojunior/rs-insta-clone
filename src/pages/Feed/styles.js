ea20f39b1b97dbab94ad26613f3d86ff33f66e16
Lázaro Nunes de Macêdo Júnior <lazarojunior@gmail.com>
2020-02-24 23:27:04 -0300

    Add style css

--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,20 +1,28 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 export const Post = styled.View`
-  
+    margin-top: 10px;
 `;
 export const Header = styled.View`
-  
+    padding: 15px;
+    flex-direction: row;
+    align-items: center;
 `;
-export const Avatar = styled.View`
-  
+export const Avatar = styled.Image`
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+    margin-right: 10px;
 `;
-export const Name = styled.View`
-  
+export const Name = styled.Text`
+    color: #333;
+    font-weight: bold;
 `;
-export const PostImage = styled.View`
-  
+export const PostImage = styled.Image`
+    width: 100%;
+    aspect-ratio: ${props => props.ratio};
 `;
-export const Description = styled.View`
-  
+export const Description = styled.Text`
+    padding: 15px;
+    line-height: 18px;
 `;
