af1c5035474f028c8aec40c2c42dba2c8f224a6f
Lázaro Nunes de Macêdo Júnior <lazarojunior@gmail.com>
2020-03-04 00:33:53 -0300

    add informacao no app

--- App.js
+++ App.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import { StatusBar, NativeModules } from 'react-native';
 
+import { AppearanceProvider } from 'react-native-appearance';
+import * as Device from 'expo-device';
+
 import Routes from './src/routes'
 
 export default function App() {
@@ -10,11 +13,30 @@ export default function App() {
   //   NativeModules.DevSettings.setIsDebuggingRemotely(true)
   // }
 
+  var envMobile = {
+    "isDevice": Device.isDevice,
+    "brand": Device.brand,
+    "deviceName": Device.deviceName,
+    "modelName": Device.modelName,
+    "modelId": Device.modelId,
+    "osName": Device.osName,
+    "osVersion": Device.osVersion,
+    "osBuildId": Device.osBuildId,
+    "platformApiLevel": Device.platformApiLevel,
+  }
+  console.log(envMobile)
+
+  // console.log(Device.brand, Device.deviceName)
+  // console.log(Device.modelId, Device.modelName)
+  // console.log('OS: ' + Device.osVersion,
+  //             'osBuild: ' + Device.osBuildId,
+  //             'Level: ' + Device.platformApiLevel)
+
   return (
-    <>
+    <AppearanceProvider>
       <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
       <Routes/>
-    </>
+    </AppearanceProvider>
   );
 }
 
